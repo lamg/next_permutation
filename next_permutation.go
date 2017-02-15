3a3191d8b649dffa2c3bc8c4f0d963d0bcd7a6b3
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2017-02-14 23:23:43 -0500

    Documentation,flags and last permutation detection

--- next_permutation.go
+++ next_permutation.go
@@ -1,18 +1,25 @@
+// Dijkstra's next permutation algorithm
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 )
 
-var usage = "Usage: %s string"
-
 func main() {
-	if len(os.Args) == 2 {
-		s := []rune(os.Args[1])
+	var str = flag.String("s", "1234", "string to find next permutation")
+	flag.Parse()
+	s := []rune(*str)
+	b := len(s) > 0
+	for i := 1; b && i != len(s); i++ {
+		b = s[i-1] >= s[i]
+	}
+	if !b {
 		next_permutation(s)
 		println(string(s))
-	}	else {
-		fmt.Fprintf(os.Stderr, usage, os.Args[0])
+	} else {
+		fmt.Fprint(os.Stderr, "Last permutation has no next")
 	}
 }
 
@@ -33,31 +40,8 @@ func next_permutation(p []rune) {
 	i, j = i+1, len(p)-1
 	for i < j {
 		p[i], p[j] = p[j], p[i]
-		i, j = i + 1, j - 1
+		i, j = i+1, j-1
 	}
 	//tail is ascending (by reversing it)
 	//p is the next permutation of the old p
-}
-
-func previous_permutation(p []rune){
-	//p is not the first one
-	i := len(p) - 2
-	for p[i] <= p[i+1] {
-		i--
-	}
-	//max i such p[i] > p[i+1]
-	j := len(p) - 1
-	for p[j] >= p[i] {
-		j--
-	}
-	//p[j] = max x: x is in p[i+1]: x
-	p[i], p[j] = p[j], p[i]
-	//p has the head up to i of the previous permutation
-	i, j = i+1, len(p) - 1
-	for i < j {
-		p[i], p[j] = p[j], p[i]
-		i, j = i + 1, j - 1
-	}
-	//tail is descending (by reversing it)
-	//p is the previous permutation of the old p
 }
